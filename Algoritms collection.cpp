f1887ea3c3d08a3bcec736f29802d2c11dcc5017
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2021-09-21 04:33:23 -0700

    up

--- Algoritms collection.cpp
+++ Algoritms collection.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main()
 {
-    int nums[]{ 2, 5, 7, 9, 10, 14, 21, 32, 36, 41, 50 }; // given array 10
+    int nums[]{ 2, 5, 7, 9, 10, 14, 21, 32, 36, 41, 50 }; // given array 10 elements
     int target_1{ 50 }; // target TRUE
     int target_2{ 12 }; // target FALSE
 
@@ -18,6 +18,7 @@ int main()
     cout << BinarySearchIteraive(nums, length, target_1);
     cout << "\n------------------------------------\n";
     cout << BinarySearchRecursive(nums, low, high, target_1);
-    
+    //Iterative returns 0 or -1 if TRUE or FALSE, 
+    //recursive implementation returns index if found element and -1 if not.
 }
 
