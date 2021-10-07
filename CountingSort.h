b23cd2fd6cea4337d7778e52f45db302ea41ce20
Michael P <30189184+MichaelPokotskyi@users.noreply.github.com>
2021-10-07 05:16:46 -0700

    fixed CountingSort

--- CountingSort.h
+++ CountingSort.h
@@ -9,11 +9,11 @@ using namespace std;
 void countsort(int arr[], int n, int k)
 {
     // create an integer array of size `n` to store the sorted array
-    int *output = new int [n];
+    int* output = new int [n]{0};
 
     // create an integer array of size `k + 1`, initialized by all zero
-    int *freq = new int [k + 1];
-    memset(freq, 0, sizeof(freq));
+    int* freq = new int[k + 1]{0};
+    //memset(freq, 0, sizeof(freq));
 
     // 1. Using the value of each item in the input array as an index,
     // store each integer's count in `freq[]`
@@ -30,7 +30,8 @@ void countsort(int arr[], int n, int k)
         total += oldCount;
     }
 
-    // 3. Copy to the output array, preserving the order of inputs with equal keys
+    // 3. Copy to the output array, 
+    // preserving the order of inputs with equal keys
     for (int i = 0; i < n; i++)
     {
         output[freq[arr[i]]] = arr[i];
@@ -41,4 +42,6 @@ void countsort(int arr[], int n, int k)
     for (int i = 0; i < n; i++) {
         arr[i] = output[i];
     }
+    delete [] output;
+    delete [] freq;
 }
